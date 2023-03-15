f9d7726a9276e4188b9abfac76d8384b03d7f74b
Bernardo Rocha <BDROCHA@Globe.life>
2023-03-14 21:19:10 -0500

    third commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,38 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http'
 import { RouterModule } from '@angular/router';
 
-
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './products/product-list.component';
-import { FormsModule } from '@angular/forms';
-import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
-import { StarComponent } from './shared/star.component';
 import { QuestionListComponent } from './questions/question-list.component';
-import { ProductDetailComponent } from './products/product-detail.component';
 import { WelcomeComponent } from './home/welcome.component';
+import { ProductModule } from './products/product.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     WelcomeComponent,
-    ProductListComponent,
-    ConvertToSpacesPipe,
-    StarComponent,
-    QuestionListComponent,
-    ProductDetailComponent
+    QuestionListComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     HttpClientModule,
     RouterModule.forRoot([
-      {path: 'products', component: ProductListComponent },
-      {path: 'products/:id', component: ProductDetailComponent },
       {path: 'welcome', component: WelcomeComponent },
       {path: 'questions', component: QuestionListComponent },
       {path: '', redirectTo: 'welcome', pathMatch: 'full'},
       {path: '**', redirectTo: 'welcome', pathMatch: 'full'}
-    ])
+    ]),
+    ProductModule
   ],
   bootstrap: [AppComponent]
 })
